11cb458c1f230a3c904f53f5855c3a167c08d0bf
Rúben Soares da Silva <rsilva@allbesmart.pt>
2024-04-24 12:05:48 +0100

    Fix packing/unpacking procedures for START.response
    Add utility functions related to START.response

--- nfapi/open-nFAPI/fapi/inc/nr_fapi_p5_utils.h
+++ nfapi/open-nFAPI/fapi/inc/nr_fapi_p5_utils.h
@@ -42,17 +42,20 @@ bool compare_param_response(const nfapi_nr_param_response_scf_t *unpacked_req, c
 bool compare_config_request(const nfapi_nr_config_request_scf_t *unpacked_req, const nfapi_nr_config_request_scf_t *req);
 bool compare_config_response(const nfapi_nr_config_response_scf_t *unpacked_req, const nfapi_nr_config_response_scf_t *req);
 bool compare_start_request(const nfapi_nr_start_request_scf_t *unpacked_req, const nfapi_nr_start_request_scf_t *req);
+bool compare_start_response(const nfapi_nr_start_response_scf_t *unpacked_req, const nfapi_nr_start_response_scf_t *req);
 
 void free_param_request(nfapi_nr_param_request_scf_t *msg);
 void free_param_response(nfapi_nr_param_response_scf_t *msg);
 void free_config_request(nfapi_nr_config_request_scf_t *msg);
 void free_config_response(nfapi_nr_config_response_scf_t *msg);
 void free_start_request(nfapi_nr_start_request_scf_t *msg);
+void free_start_response(nfapi_nr_start_response_scf_t *msg);
 
 void copy_param_request(const nfapi_nr_param_request_scf_t *src,  nfapi_nr_param_request_scf_t *dst);
 void copy_param_response(const nfapi_nr_param_response_scf_t *src,  nfapi_nr_param_response_scf_t *dst);
 void copy_config_request(const nfapi_nr_config_request_scf_t *src,  nfapi_nr_config_request_scf_t *dst);
 void copy_config_response(const nfapi_nr_config_response_scf_t *src,  nfapi_nr_config_response_scf_t *dst);
 void copy_start_request(const nfapi_nr_start_request_scf_t *src,  nfapi_nr_start_request_scf_t *dst);
+void copy_start_response(const nfapi_nr_start_response_scf_t *src,  nfapi_nr_start_response_scf_t *dst);
 
 #endif // OPENAIRINTERFACE_NR_FAPI_P5_UTILS_H
--- nfapi/open-nFAPI/fapi/src/nr_fapi_p5_utils.c
+++ nfapi/open-nFAPI/fapi/src/nr_fapi_p5_utils.c
@@ -452,6 +452,14 @@ bool compare_start_request(const nfapi_nr_start_request_scf_t *unpacked_req, con
   return 0;
 }
 
+bool compare_start_response(const nfapi_nr_start_response_scf_t *unpacked_req, const nfapi_nr_start_response_scf_t *req)
+{
+  CMP(unpacked_req->header.message_id, req->header.message_id);
+  CMP(unpacked_req->header.message_length, req->header.message_length);
+  CMP(unpacked_req->error_code, req->error_code);
+  return 0;
+}
+
 void free_param_request(nfapi_nr_param_request_scf_t *msg)
 {
   if (msg->vendor_extension) {
@@ -522,6 +530,13 @@ void free_config_response(nfapi_nr_config_response_scf_t *msg)
 }
 
 void free_start_request(nfapi_nr_start_request_scf_t *msg)
+{
+  if (msg->vendor_extension) {
+    free(msg->vendor_extension);
+  }
+}
+
+void free_start_response(nfapi_nr_start_response_scf_t *msg)
 {
   if (msg->vendor_extension) {
     free(msg->vendor_extension);
@@ -1055,4 +1070,17 @@ void copy_start_request(const nfapi_nr_start_request_scf_t *src, nfapi_nr_start_
     dst->vendor_extension->length = src->vendor_extension->length;
     // TODO: FIGURE OUT WHERE THE VENDOR EXTENSION VALUE IS
   }
+}
+
+void copy_start_response(const nfapi_nr_start_response_scf_t *src, nfapi_nr_start_response_scf_t *dst)
+{
+  dst->header.message_id = src->header.message_id;
+  dst->header.message_length = src->header.message_length;
+  if (src->vendor_extension) {
+    dst->vendor_extension = calloc(1, sizeof(nfapi_vendor_extension_tlv_t));
+    dst->vendor_extension->tag = src->vendor_extension->tag;
+    dst->vendor_extension->length = src->vendor_extension->length;
+    // TODO: FIGURE OUT WHERE THE VENDOR EXTENSION VALUE IS
+  }
+  dst->error_code = src->error_code;
 }
